9f58613f775acc0281d10f06688505a3794ca840
Kevbot718 <kstevens7@utexas.edu>
2017-04-27 19:44:36 -0500

    HomeController description

--- FinalGroupProjectTeam8/Controllers/HomeController.cs
+++ FinalGroupProjectTeam8/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace FinalGroupProjectTeam8.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page goes here.";
 
             return View();
         }
