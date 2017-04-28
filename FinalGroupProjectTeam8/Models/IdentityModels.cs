dfb6d1752ef6b6ac431aa5f18944ffa680da44d1
Kevbot718 <kstevens7@utexas.edu>
2017-04-27 20:22:09 -0500

    New Identity bank user model. Not 100%

--- FinalGroupProjectTeam8/Models/IdentityModels.cs
+++ FinalGroupProjectTeam8/Models/IdentityModels.cs
@@ -3,17 +3,64 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
 
 namespace FinalGroupProjectTeam8.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+    public enum UserTypeEnum { Customer, Employee, Manager }
+
     public class AppUser : IdentityUser
     {
 
-        //TODO: Put any additional fields that you need for your user here
-        //For instance
-        public string FName { get; set; }
+        // Bank user scalar and navigational properties
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First name is required.")]
+        public String FirstName { get; set; }
 
+        [Display(Name = "Middle Initial")]
+        [Required(ErrorMessage = "Middle initial is required.")]
+        public String MiddleInitial { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is required.")]
+        public String Password { get; set; }
+
+        [Display(Name = "Birthday")]
+        [Required(ErrorMessage = "Birthday is required.")]
+        public DateTime Birthday { get; set; }
+
+        [Display(Name = "Street")]
+        [Required(ErrorMessage = "Street is required.")]
+        public String Street { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "City is required.")]
+        public String City { get; set; }
+
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "State is required.")]
+        public String State { get; set; }
+
+        [Display(Name = "Zipcode")]
+        [Required(ErrorMessage = "Zipcode is required.")]
+        public String Zip { get; set; }
+
+        public UserTypeEnum UserType { get; set; }
+
+        public virtual CheckingAccount CheckingAccount { get; set; }
+
+        public virtual SavingsAccount SavingsAccount { get; set; }
+
+        public virtual List<IRA> IRAs { get; set; }
+        public virtual List<StockPortfolio> StockPortfolios { get; set; }
+        public virtual List<Deposit> Deposits { get; set; }
+        public virtual List<Transfer> Transfers { get; set; }
+        public virtual List<Payment> Payments { get; set; }
 
         //This method allows you to create a new user
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
@@ -31,9 +78,6 @@ namespace FinalGroupProjectTeam8.Models
         //TODO:  Add dbsets here, for instance there's one for books
         //Remember, Identity adds a db set for users, so you shouldn't add that one - you will get an error
         //public DbSet<Book> Books { get; set; }
-
-        public DbSet<BankUser> BankUsers { get; set; }
-
 
         //TODO: Make sure that your connection string name is correct here.
         public AppDbContext()
